3d7528aa644e8d07c68a6c8fa2a5910e559e26a8
Clémence <98712982+clemenceprv@users.noreply.github.com>
2023-01-24 09:19:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // TYPED
 var typed = new Typed('.typed', {
-  strings: ["Bonjour ! Actuellement en quatrième année en école d'ingénieur informatique, je souhaite réaliser mon alternance pour ma dernière année à Polytech Lyon. Je suis motivée pour approfondir mes connaissances et apprendre de nouvelles compétences. Au terme de cette alternance, j'aimerais profiter de l'expérience accumulée pour poursuivre avec vous en CDI. Je cherche de préférence à Nantes ou Lyon, mais je suis ouverte aux déplacements dans toute la France."],
+  strings: ["Bonjour ! Actuellement en quatrième année en école d'ingénieur informatique, je souhaite réaliser ma dernière année à Polytech Lyon en alternance. Je suis motivée pour approfondir mes connaissances et apprendre de nouvelles compétences. Au terme de cette alternance, j'aimerais profiter de l'expérience accumulée pour poursuivre avec vous en CDI. Je cherche de préférence à Nantes ou Lyon, mais je suis ouverte aux déplacements dans toute la France."],
   typeSpeed: 15,
 });
   
